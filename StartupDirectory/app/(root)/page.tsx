65d90dbf9f6d5d42acb6561c6b81fb6732ebf0ed
Shantanu Ingale <singale@medisolv.com>
2025-02-20 11:41:57 +0530

    1. Adding records for Author and Startup in "lh/studio" using its UI 2. Visualizing the data using Sanity's GROQ os query language "lh/studio/vision" 3. Creating queries for GROQ independently in app "/sanity/lib/queries.ts" 4. Doing automatic type-safety (StatupTypeCard type) using Sanity's TypeGen

--- StartupDirectory/app/(root)/page.tsx
+++ StartupDirectory/app/(root)/page.tsx
@@ -1,5 +1,7 @@
+import { client } from "@/sanity/lib/client";
 import SearchForm from "../../components/SearchForm";
-import StartupCard from "../../components/StartupCard";
+import StartupCard, { StartupTypeCard } from "../../components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home(
     {searchParams}:{searchParams: Promise<{query?:string}>}
@@ -7,18 +9,7 @@ export default async function Home(
 
     const query = (await searchParams).query;
 
-    const posts =[
-      {
-        _createdAt: new Date(2025, 1, 12, 7, 30, 34),
-        _id:1,
-        views: 55,
-        author: {_id:1001, name:"Ved Vyas"},
-        description: "Mahabharata is an encient Hindu scripture, that specifies an huge war of dwaparyug named over the book. The war was fought between Pandavas, the pandu putras and Kauravas, the kaurav putras. It describes how this great was fought and how a legend (God in Hindu) Shree Krishna had his hand over the war. The war is also called as Dharm Yudh, where pandas and krishna from dharm and kauravas and other warrior on their side from adharm.",
-        image: "https://img.freepik.com/premium-photo/lord-krishna-arjuna-chariot_669954-60304.jpg?w=740",
-        category: "Hindu Mahagrantha",
-        title: "Mahabharata"
-      }
-    ];
+    const posts = await client.fetch(STARTUPS_QUERY);
 
     return(
       <>
@@ -38,8 +29,8 @@ export default async function Home(
             {
               posts.length > 0
               ? (
-                posts.map((post: StartupTypeCard, index:number)=>(
-                  <StartupCard key={post?.id} post={post} />
+                posts.map((post: StartupTypeCard)=>(
+                  <StartupCard key={post?._id} post={post} />
                 ))
               ) : (
                 <p className="no-results">No Startups found!!</p>
--- StartupDirectory/components/StartupCard.tsx
+++ StartupDirectory/components/StartupCard.tsx
@@ -4,65 +4,75 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
+import { Author, Startup } from '@/sanity/types'
+
+// using typegen, Author and Startup types are created automatically in /sanity/types
+export type StartupTypeCard = Omit<Startup, "author"> & {author?: Author}
+
 
 const StartupCard = (
     {post}:{post:StartupTypeCard}
 ) => {
-  return (
-    <li className="startup-card group">
-        
-        <div className="flex-between">
-            <p className="startup_card_date">
-                { formatDate(post._createdAt) }
-            </p>
-            <div className="flex gap-1.5">
-                <EyeIcon className='size-6 text-primary' />
-                <span className="text-16-medium">{post.views}</span>
-            </div>
-        </div>
 
-        <div className="flex-between mt-5 gap-5">
-            <div className="flex-1">
-                <Link href={`user/${post.author._id}`}>
-                    <p className="text-16-medium line-clamp-1"> {post.author.name} </p>
-                </Link>
-                <Link href={`/startup/${post._id}`}>
-                    <h3 className="text-26-semibold line-clamp-1">{post.title}</h3>
-                </Link>
-            </div>
+    const {_createdAt, views, title, category, _id, image, description, author} = post;
 
-            <Link href={`user/${post.author._id}`}>
-                <Image src={post.image} alt={post.title} width={48} height={48} 
-                 className='rounded-full'/>
-            </Link>
-        </div>
-
-        <Link href={`/startup/${post._id}`}>
-            <p className="startup-card_desc">
-                {post.description}
-            </p>
-
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={post.image} alt="Placeholder" className='startup-card_img' />
-            {/* img does work we need w/custom css but next's <Image> is not that suitable here */}
-        </Link>
-
-        <div className="flex-between gap-3 mt-5">
-            <Link href={`/?query=${post.category.toLowerCase()}`}>
-                <p className="text-16-medium">
-                    {post.category}
+    return (
+        <li className="startup-card group">
+            
+            <div className="flex-between">
+                <p className="startup_card_date">
+                    {/* using formatDate from utils that we made custom to return local date string*/}
+                    { formatDate(_createdAt) }
                 </p>
+                <div className="flex gap-1.5">
+                    <EyeIcon className='size-6 text-primary' />
+                    <span className="text-16-medium">{views}</span>
+                </div>
+            </div>
+
+            <div className="flex-between mt-5 gap-5">
+                <div className="flex-1">
+                    <Link href={`user/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1"> {author?.name} </p>
+                    </Link>
+                    <Link href={`/startup/${_id}`}>
+                        <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+                    </Link>
+                </div>
+
+                <Link href={`user/${author?._id}`}>
+                    {/* ! mark determines to TS that, property may look null but it will surely hold a value */}
+                    <Image src={image!} alt={title!} width={48} height={48} 
+                    className='rounded-full'/>
+                </Link>
+            </div>
+
+            <Link href={`/startup/${_id}`}>
+                <p className="startup-card_desc">
+                    {description}
+                </p>
+
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={image} alt="Placeholder" className='startup-card_img' />
+                {/* img does work we need w/custom css but next's <Image> is not that suitable here */}
             </Link>
 
-            <Button className='startup-card_btn' asChild>
-                <Link href={`/startup/${post._id}`}>
-                    Details
+            <div className="flex-between gap-3 mt-5">
+                <Link href={`/?query=${category?.toLowerCase()}`}>
+                    <p className="text-16-medium">
+                        {category}
+                    </p>
                 </Link>
-            </Button>
-        </div>
-        
-    </li>
-  )
+
+                <Button className='startup-card_btn' asChild>
+                    <Link href={`/startup/${_id}`}>
+                        Details
+                    </Link>
+                </Button>
+            </div>
+            
+        </li>
+    )
 }
 
 export default StartupCard
